4bb14203bbb2df75379d9cf55807bba1e6f7a85d
BrunoHebert <bhr@icomp.ufam.edu.br>
2017-02-01 09:43:31 -0400

    cod

--- php1/processa.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  echo "Nome: " . $_POST["nome"] . "</br>";
-  echo "Email: " . $_POST["email"] . "</br>";
-  echo "Website: " . $_POST["website"] . "</br>";
-  echo "Mensagem: " . $_POST["mensagem"] . "</br>";
-
- ?>

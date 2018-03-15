3504b25cc4c84a2177bc3daa4aa59438e756d445
icomp <bhr@icomp.ufam.edu.br>
2018-03-15 15:55:37 -0400

    lab1

--- TP/Lab1/OperacoesInteiros.java
+++ TP/Lab1/OperacoesInteiros.java
@@ -8,27 +8,50 @@ public class OperacoesInteiros {
 
 		Scanner scan = new Scanner(System.in);
 		ArrayList<Integer> vetor = new ArrayList<Integer>();
+
+		ArrayList<Integer> vetorSize = new ArrayList<Integer>();
+		ArrayList<Integer> Par = new ArrayList<Integer>();
+		ArrayList<Integer> Impar = new ArrayList<Integer>();
+		ArrayList<Integer> Sum = new ArrayList<Integer>();
+		ArrayList<Float> Med = new ArrayList<Float>();
+		ArrayList<Integer> Maior = new ArrayList<Integer>();
+		ArrayList<Integer> Menor = new ArrayList<Integer>();
+
 		int entrada = 0;
+		int indice=0;
+		int controlador = 0;
 		do{
-		int  contPar =0, contImpar = 0, soma = 0, maior = 0, menor = 0;
-		entrada = 0;
-		while (entrada!=-1){
-			entrada = scan.nextInt();
-			if(entrada!=-1){ 
-				if(vetor.size()<1)menor = entrada;
-				vetor.add(entrada);
-				soma = entrada + soma;
-				if(maior<entrada){
-					maior = entrada;
-				}
-				if(menor>entrada){
-					menor = entrada;
+			int  contPar =0, contImpar = 0, soma = 0, maior = 0, menor = 0;
+			entrada = 0;
+
+			while (entrada!=-1){
+				entrada = scan.nextInt();
+				/*Fazer verificação de quem e o maior valor e o menor valor*/
+
+				if(entrada!=-1){
+					if(vetor.size()<1)menor = entrada;
+					vetor.add(entrada);
+					soma = entrada + soma;
+					if(maior<entrada){
+						maior = entrada;
+					}
+					if(menor>entrada){
+						menor = entrada;
+					}
+					if(entrada %2 ==0){
+						contPar = contPar+1;
+					}
+					else{
+						contImpar = contImpar+1;
+					}
 				}
 			}
-		}
-		
-		float media = soma/vetor.size();
-		for(Integer num : vetor){
+			controlador = (vetor.size()) - controlador;
+		//	System.out.printf("Tamanho do vetor: %d\n controlador: %d\n ", vetor.size(), controlador);
+
+			float media = soma/controlador; //obter media dos valores
+			/*Obter quantidade de numeros pares e impares*/
+		/*	for(Integer num : vetor){
 			if(num %2 ==0){
 				contPar = contPar+1;
 			}
@@ -36,31 +59,33 @@ public class OperacoesInteiros {
 				contImpar = contImpar+1;
 			}
 		}
-		ArrayList<Integer> vetorSize = new ArrayList<Integer>();
-		vetorSize.add(vetor.size());
-		
-		ArrayList<Integer> Par = new ArrayList<Integer>();
-		Par.add(contPar);
-		
-		ArrayList<Integer> Impar = new ArrayList<Integer>();
-		Impar.add(contImpar);
-		
-		ArrayList<Integer> Sum = new ArrayList<Integer>();
-		Sum.add(soma);
-		
-		ArrayList<Float> Med = new ArrayList<Float>();
-		Med.add(media);
-		
-		ArrayList<Integer> Maior = new ArrayList<Integer>();
-		Maior.add(maior);
-		
-		ArrayList<Integer> Menor = new ArrayList<Integer>();
-		Menor.add(menor);
-		
-			
-	}while(vetor.size()!=0);
-		
-		for()
-}		
+				/*Vetores separados contendo tamanho de cada vetor, qntd pares, impares, media, soma... */
+				vetorSize.add(controlador);
 
+				Par.add(contPar);
+
+				Impar.add(contImpar);
+
+				Sum.add(soma);
+
+				Med.add(media);
+
+				Maior.add(maior);
+
+				Menor.add(menor);
+
+					System.out.println(vetorSize.get(indice));
+					System.out.println(Par.get(indice));
+					System.out.println(Impar.get(indice));
+					System.out.println(Sum.get(indice));
+					System.out.println(Med.get(indice));
+					System.out.println(Maior.get(indice));
+					System.out.println(Menor.get(indice));
+
+
+				indice = indice+1;
+			}while(controlador!=0);
+
+
+		}
 }
